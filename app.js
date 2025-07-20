0e3b8844720db11f8a9b6e14b61ed9fc797543e5
nur alam joy <ajnuralam@gmail.com>
2025-07-20 16:49:49 +0600

    added crnt age

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let name = document.getElementById("name");
 let result = document.getElementById("result");
 let btn = document.getElementById("btn");
 let person = document.getElementById("person");
+let age = document.getElementById("age");
 function myFunc() {
   const year = parseInt(document.getElementById("year").value);
   const month = parseInt(document.getElementById("month").value);
@@ -14,19 +15,28 @@ function myFunc() {
   }
 
   let nextBD = new Date(today.getFullYear(), month, day);
-
+  let birthDate = new Date(year, month, day);
+  let crntAge = today - birthDate;
+  crntAge = Math.floor(Math.ceil(crntAge / (1000 * 60 * 60 * 24)) / 365);
   if (nextBD < today) {
     nextBD.setFullYear(today.getFullYear() + 1);
   }
 
+  person.innerText = `🎂 Hi ${name.value} 🎂`;
+  if (crntAge === 0) {
+    crntAge = today - birthDate;
+    crntAge = Math.floor(Math.ceil(crntAge / (1000 * 60 * 60 * 24)) / 30);
+    age.innerText = `Your are now only ${crntAge} month's old!`;
+  } 
+  else {
+    age.innerText = `Your are now ${crntAge} year's old!`;
+  }
   if (today.getDate() === day && today.getMonth() === month) {
-    person.innerText = `Hi ${name.value}`;
     result.innerText = `🎉 Happy Birthday 🎉`;
   } 
   else {
     const diffTime = nextBD - today;
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    person.innerText = `🎂 Hi ${name.value} 🎂`;
     result.innerHTML = `Your next birthday is in ${diffDays} days on <br>${nextBD.toDateString()}.`;
   }
 }
